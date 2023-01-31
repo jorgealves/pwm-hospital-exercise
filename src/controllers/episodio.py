a5ec90a4838496ebe775fcf589b5d5485216a80a
JamieBurridge <xross_dev@proton.me>
2023-01-31 19:26:17 +0000

    get senhas and episodios

--- src/controllers/episodio.py
+++ src/controllers/episodio.py
@@ -2,3 +2,6 @@ episodios = []
 
 def abrir_episodio(senha: dict, observacoes: str):
     return episodios.append({senha: senha, observacoes: observacoes})
+
+def var_episodios():
+    return episodios
--- src/controllers/senha.py
+++ src/controllers/senha.py
@@ -3,3 +3,5 @@ senhas = []
 def adicionar_senha (numero: int, id_utente: int):    
     return senhas.append({numero: numero, id_utente: id_utente})
 
+def ver_senhas():
+    return senhas
